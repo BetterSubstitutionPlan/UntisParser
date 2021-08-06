b5290390938897980a66df729c6c7d7b473ddfa8
minie4 <minie4@gmx.de>
2021-08-06 12:37:52 +0200

    🐛 🎨 Fix crash and refactor code
    - Fix crash if there are no changes
    - Return infos object instead of the data array
    - Reformat all date stings
    - Add leading zeros to all date strings
    - Refactor code

--- substitutionplan.js
+++ substitutionplan.js
@@ -2,50 +2,47 @@ const cheerio = require('cheerio');
 const titles = ["klasse", "datum", "stunde", "lehrer", "vertreter", "fach", "raum", "art", "text"];
 
 function parse(html) {
-    // Load the html
-    var $ = cheerio.load(html);
-    var data = [];
     var infos = {};
 
+    // Load the html into cheerio
+    var $ = cheerio.load(html);
+
+    // Extract the date, weekday and a/b week from the title
     title = $('.mon_title').text().split(" ");
-    infos.day = title[1]
-    infos.date = title[0]
+    infos.date = title[0].split(".").reverse().map(x => (x.length < 2) ? "0" + x : x).join("-")
+    infos.day = title[1].replace(",", "")
     infos.week = title[3]
-    infos.stand = "Stand" + $('.mon_head td').text().split("Stand")[1].replace("\\n", "").replace(/[\s]*$/, "");
 
+    // Get the export timestamp
+    infos.timestamp = $('.mon_head td').text().split("Stand: ")[1].replace(/[\s\n]*$/g, "");
+
+    var data = [];
     list = $('table.mon_list tr.list');
-    rowcnt = 0;
-    list.each(function () {
-        data[rowcnt] = {};
+    list.each((rowcnt, row) => {
+        var rowData = {};
 
-        var arr = $(this).find("td")
+        var columns = $(row).find("td")
+        if (columns.text() == "") return;
+        if (columns.text().includes("Keine Vertretungen")) return;
 
-        if (arr.text() == "") {
-            return;
-        }
-
-        arr.each((spancnt, element) => {
-            text = $(element).text()
+        columns.each((columncnt, column) => {
+            text = $(column).text()
             cleantext = text.replace("\\n", "").replace(/[\s]*$/, "")
-            data[rowcnt][titles[spancnt]] = cleantext;
+            rowData[titles[columncnt]] = cleantext;
         })
 
-        rowcnt++;
+        data.push(rowData)
     });
     infos.changes = data;
 
-    data.forEach((element) => {
-        let data = element.datum;
-        element.datum = "";
-
-        data = data.split(".")
-
-        element.datum += new Date().getFullYear() + "-"
-        element.datum += data[1] + "-"
-        element.datum += data[0]
+    // Reformat date
+    data.forEach(element => {
+        if(!element.datum) return;
+        element.datum += infos.date.split("-")[0]
+        element.datum = element.datum.split(".").reverse().map(x => (x.length < 2) ? "0" + x : x).join("-")
     })
 
-    return data;
+    return infos;
 }
 
 module.exports = {
